aa569f85c4140ae12550dc8744c21bbf65932905
Joshtri <stuffofyos1516@gmail.com>
2024-12-22 09:30:11 +0800

    adding user route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,27 +1,27 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller";
 
-const userRoutes = Router();
+const userRoute = Router();
 const userController = new UserController();
 // Define routes for User
-userRoutes.get("/users", (req, res) => {
+userRoute.get("/users", (req, res) => {
     userController.getAllUsers(req, res);
 });
 
-userRoutes.get("/users/:id", (req, res) => {
+userRoute.get("/users/:id", (req, res) => {
     userController.getUserById(req, res)
 });
 
-userRoutes.post("/users", (req, res) => {
+userRoute.post("/users", (req, res) => {
     userController.createUser(req, res)
 });
 
-userRoutes.put("/users/:id", (req, res) => {
+userRoute.put("/users/:id", (req, res) => {
     userController.updateUser(req, res)
 });
 
-userRoutes.delete("/users/:id", (req, res) => {
+userRoute.delete("/users/:id", (req, res) => {
     userController.deleteUser(req, res)
 });
 
-export default userRoutes;
+export default userRoute;
